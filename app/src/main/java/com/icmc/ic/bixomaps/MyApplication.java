d408e2204f9892f007e684a9a2ad8d8b247c7478
Caio Lopes <caiodesalopes@gmail.com>
2016-09-18 14:00:20 -0300

    Changes on API keys security

--- app/src/main/java/com/icmc/ic/bixomaps/MyApplication.java
+++ app/src/main/java/com/icmc/ic/bixomaps/MyApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.support.v7.app.AppCompatDelegate;
 
 import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -11,7 +12,13 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Fabric.with(this, new Crashlytics());
+        // Set up Crashlytics, disabled for debug builds
+        Crashlytics crashlyticsKit = new Crashlytics.Builder()
+                .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+                .build();
+
+        // Initialize Fabric with the debug-disabled crashlytics.
+        Fabric.with(this, crashlyticsKit);
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
     }
 }
